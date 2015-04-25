c9b76cb1f93cb131d60a26128761a988bce69dc4
unknown <slf.pro@gmail.com>
2015-04-25 19:19:36 +0200

    modification details login page

--- app/views/pages/login.blade.php
+++ app/views/pages/login.blade.php
@@ -10,10 +10,8 @@ Like: www.facebook.com/keenthemes
 Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
 License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
 -->
-<!--[if IE 8]>
-<html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 9]>
-<html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
 <!--[if !IE]><!-->
 <html lang="en">
 <!--<![endif]-->
@@ -39,170 +37,154 @@ License: You must have a valid license purchased only from themeforest(the above
 <div class="content">
     <!-- BEGIN LOGIN FORM -->
     {{ Form::open(array('url' => 'auth/login', 'method' => 'post', 'class' => 'login-form')) }}
-    <h3 class="form-title">Se connecter</h3>
 
-    <div class="alert alert-danger display-hide">
-        <button class="close" data-close="alert"></button>
+    <h3 class="form-title">Se connecter</h3>
+        <div class="alert alert-danger display-hide">
+            <button class="close" data-close="alert"></button>
 			<span>
 			Enter any username and password. </span>
-    </div>
-    <div class="form-group">
-        <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-        <label class="control-label visible-ie8 visible-ie9">Nom</label>
-        <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off"
-               placeholder="Username" name="username"/>
-    </div>
-    <div class="form-group">
-        <label class="control-label visible-ie8 visible-ie9">Mot de passe</label>
-        <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off"
-               placeholder="Password" name="password"/>
-    </div>
-    <div class="form-actions">
-        <button type="submit" class="btn btn-success uppercase">GO !</button>
+        </div>
+        <div class="form-group">
+            <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+            <label class="control-label visible-ie8 visible-ie9">Nom</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"/>
+        </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Mot de passe</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password"/>
+        </div>
+        <div class="form-actions">
+            <button type="submit" class="btn btn-success uppercase">Login</button>
+            <label class="rememberme check">
+                <input type="checkbox" name="remember" value="1"/>Remember </label>
+            <a href="javascript:;" id="forget-password" class="forget-password">Forgot Password?</a>
+        </div>
 
-        <a href="javascript:;" id="forget-password" class="forget-password">Forgot Password?</a>
-    </div>
-
-    <div class="create-account">
-        <p>
-            <a href="javascript:;" id="register-btn" class="uppercase">Creer un compte</a>
-        </p>
-        {{ Form::close() }}
-
-        <!-- END LOGIN FORM -->
-        <!-- BEGIN FORGOT PASSWORD FORM -->
-        <form class="forget-form" action="index.html" method="post">
-            <h3>Mot de passe oublié ?</h3>
-
+        <div class="create-account">
             <p>
-                Entrez votre email.
+                <a href="javascript:;" id="register-btn" class="uppercase">Creer un compte</a>
             </p>
+       </div>
+    {{ Form::close() }}
 
-            <div class="form-group">
-                <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Email"
-                       name="email"/>
+    <!-- END LOGIN FORM -->
+    <!-- BEGIN FORGOT PASSWORD FORM -->
+    <form class="forget-form" action="index.html" method="post">
+        <h3>Mot de passe oublié ?</h3>
+        <p>
+            Entrez votre email.
+        </p>
+        <div class="form-group">
+            <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Email" name="email"/>
+        </div>
+        <div class="form-actions">
+            <button type="button" id="back-btn" class="btn btn-default">Retour</button>
+            <button type="submit" class="btn btn-success uppercase pull-right">Envoyer</button>
+        </div>
+    </form>
+    <!-- END FORGOT PASSWORD FORM -->
+    <!-- BEGIN REGISTRATION FORM -->
+    <form class="register-form" action="index.html" method="post">
+        <h3>Sign Up</h3>
+        <p class="hint">
+            Enter your personal details below:
+        </p>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Full Name</label>
+            <input class="form-control placeholder-no-fix" type="text" placeholder="Full Name" name="fullname"/>
+        </div>
+        <div class="form-group">
+            <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+            <label class="control-label visible-ie8 visible-ie9">Email</label>
+            <input class="form-control placeholder-no-fix" type="text" placeholder="Email" name="email"/>
+        </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Address</label>
+            <input class="form-control placeholder-no-fix" type="text" placeholder="Address" name="address"/>
+        </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">City/Town</label>
+            <input class="form-control placeholder-no-fix" type="text" placeholder="City/Town" name="city"/>
+        </div>
+        <p class="hint">
+            Enter your account details below:
+        </p>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Username</label>
+            <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"/>
+        </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Password</label>
+            <input class="form-control placeholder-no-fix" type="password" autocomplete="off" id="register_password" placeholder="Password" name="password"/>
+        </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Re-type Your Password</label>
+            <input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Re-type Your Password" name="rpassword"/>
+        </div>
+        <div class="form-group margin-top-20 margin-bottom-20">
+            <label class="check">
+                <input type="checkbox" name="tnc"/> I agree to the <a href="#">
+                    Terms of Service </a>
+                & <a href="#">
+                    Privacy Policy </a>
+            </label>
+            <div id="register_tnc_error">
             </div>
-            <div class="form-actions">
-                <button type="button" id="back-btn" class="btn btn-default">Retour</button>
-                <button type="submit" class="btn btn-success uppercase pull-right">Envoyer</button>
-            </div>
-        </form>
-        <!-- END FORGOT PASSWORD FORM -->
-        <!-- BEGIN REGISTRATION FORM -->
-        <form class="register-form" action="index.html" method="post">
-            <h3>Sign Up</h3>
-
-            <p class="hint">
-                Enter your personal details below:
-            </p>
-
-            <div class="form-group">
-                <label class="control-label visible-ie8 visible-ie9">Full Name</label>
-                <input class="form-control placeholder-no-fix" type="text" placeholder="Full Name" name="fullname"/>
-            </div>
-            <div class="form-group">
-                <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-                <label class="control-label visible-ie8 visible-ie9">Email</label>
-                <input class="form-control placeholder-no-fix" type="text" placeholder="Email" name="email"/>
-            </div>
-            <div class="form-group">
-                <label class="control-label visible-ie8 visible-ie9">Address</label>
-                <input class="form-control placeholder-no-fix" type="text" placeholder="Address" name="address"/>
-            </div>
-            <div class="form-group">
-                <label class="control-label visible-ie8 visible-ie9">City/Town</label>
-                <input class="form-control placeholder-no-fix" type="text" placeholder="City/Town" name="city"/>
-            </div>
-            <p class="hint">
-                Enter your account details below:
-            </p>
-
-            <div class="form-group">
-                <label class="control-label visible-ie8 visible-ie9">Username</label>
-                <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username"
-                       name="username"/>
-            </div>
-            <div class="form-group">
-                <label class="control-label visible-ie8 visible-ie9">Password</label>
-                <input class="form-control placeholder-no-fix" type="password" autocomplete="off" id="register_password"
-                       placeholder="Password" name="password"/>
-            </div>
-            <div class="form-group">
-                <label class="control-label visible-ie8 visible-ie9">Re-type Your Password</label>
-                <input class="form-control placeholder-no-fix" type="password" autocomplete="off"
-                       placeholder="Re-type Your Password" name="rpassword"/>
-            </div>
-            <div class="form-group margin-top-20 margin-bottom-20">
-                <label class="check">
-                    <input type="checkbox" name="tnc"/> I agree to the <a href="#">
-                        Terms of Service </a>
-                    & <a href="#">
-                        Privacy Policy </a>
-                </label>
-
-                <div id="register_tnc_error">
-                </div>
-            </div>
-            <div class="form-actions">
-                <button type="button" id="register-back-btn" class="btn btn-default">Back</button>
-                <button type="submit" id="register-submit-btn" class="btn btn-success uppercase pull-right">Submit
-                </button>
-            </div>
-        </form>
-        <!-- END REGISTRATION FORM -->
-    </div>
-    <div class="copyright">
-        2014 © Metronic. Admin Dashboard Template.
-    </div>
-    <!-- END LOGIN -->
-    <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-    <!-- BEGIN CORE PLUGINS -->
-    <!--[if lt IE 9]>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/respond.min.js')}}"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/excanvas.min.js')}}"></script>
-    <![endif]-->
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery.min.js')}}"
-            type="text/javascript"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery-migrate.min.js')}}"
-            type="text/javascript"></script>
-    <!-- IMPORTANT! Load jquery-ui.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery-ui/jquery-ui.min.js')}}"
-            type="text/javascript"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/bootstrap/js/bootstrap.min.js')}}"
-            type="text/javascript"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js')}}"
-            type="text/javascript"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js')}}"
-            type="text/javascript"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery.blockui.min.js')}}"
-            type="text/javascript"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery.cokie.min.js')}}"
-            type="text/javascript"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/uniform/jquery.uniform.min.js')}}"
-            type="text/javascript"></script>
-    <!-- END CORE PLUGINS -->
-    <!-- BEGIN PAGE LEVEL PLUGINS -->
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery-validation/js/jquery.validate.min.js')}}"
-            type="text/javascript"></script>
-    <!-- END PAGE LEVEL PLUGINS -->
-    <!-- BEGIN PAGE LEVEL SCRIPTS -->
-    <script src="{{asset('metronic_v3.6.2/theme/assets/global/scripts/metronic.js')}}" type="text/javascript"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/admin/layout3/scripts/layout.js')}}"
-            type="text/javascript"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/admin/layout3/scripts/demo.js')}}"
-            type="text/javascript"></script>
-    <script src="{{asset('metronic_v3.6.2/theme/assets/admin/pages/scripts/login.js')}}"
-            type="text/javascript"></script>
-    <!-- END PAGE LEVEL SCRIPTS -->
-    <script>
-        jQuery(document).ready(function () {
-            Metronic.init(); // init metronic core components
-            Layout.init(); // init current layout
-            Login.init();
-            Demo.init();
-        });
-    </script>
-    <!-- END JAVASCRIPTS -->
+        </div>
+        <div class="form-actions">
+            <button type="button" id="register-back-btn" class="btn btn-default">Back</button>
+            <button type="submit" id="register-submit-btn" class="btn btn-success uppercase pull-right">Submit</button>
+        </div>
+    </form>
+    <!-- END REGISTRATION FORM -->
+</div>
+<div class="copyright">
+    2014 © Metronic. Admin Dashboard Template.
+</div>
+<!-- END LOGIN -->
+<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+<!-- BEGIN CORE PLUGINS -->
+<!--[if lt IE 9]>
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/respond.min.js')}}"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/excanvas.min.js')}}"></script>
+<![endif]-->
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery-migrate.min.js')}}"
+        type="text/javascript"></script>
+<!-- IMPORTANT! Load jquery-ui.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery-ui/jquery-ui.min.js')}}"
+        type="text/javascript"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/bootstrap/js/bootstrap.min.js')}}"
+        type="text/javascript"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js')}}"
+        type="text/javascript"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js')}}"
+        type="text/javascript"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery.blockui.min.js')}}"
+        type="text/javascript"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery.cokie.min.js')}}"
+        type="text/javascript"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/uniform/jquery.uniform.min.js')}}"
+        type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/plugins/jquery-validation/js/jquery.validate.min.js')}}" type="text/javascript"></script>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="{{asset('metronic_v3.6.2/theme/assets/global/scripts/metronic.js')}}" type="text/javascript"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/admin/layout3/scripts/layout.js')}}" type="text/javascript"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/admin/layout3/scripts/demo.js')}}" type="text/javascript"></script>
+<script src="{{asset('metronic_v3.6.2/theme/assets/admin/pages/scripts/login.js')}}" type="text/javascript"></script>
+<!-- END PAGE LEVEL SCRIPTS -->
+<script>
+    jQuery(document).ready(function() {
+        Metronic.init(); // init metronic core components
+        Layout.init(); // init current layout
+        Login.init();
+        Demo.init();
+    });
+</script>
+<!-- END JAVASCRIPTS -->
 </body>
 <!-- END BODY -->
 </html>
