d788f0228064e19288f3602134067b46ca0c98ed
unknown <slf.pro@gmail.com>
2015-05-14 17:20:00 +0200

    correction erreur post iframe betbrain

--- app/views/pages/dashboard.blade.php
+++ app/views/pages/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-    {{file_put_contents('log_index.txt', json_encode($_GET) . "\n" , FILE_APPEND | LOCK_EX)}}
+    {{file_put_contents('log_index.txt', json_encode($_POST) . "\n" , FILE_APPEND | LOCK_EX)}}
     <!-- BEGIN PAGE HEAD -->
     <div class="page-head">
         <div class="container-fluid">
