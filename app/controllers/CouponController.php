69593d663d7fbd64e333a2b9eb0a941b00fb1876
zelol <slf.pro@gmail.com>
2015-05-22 11:53:42 +0200

    ajout table coupon

--- app/controllers/CouponController.php
+++ app/controllers/CouponController.php
@@ -116,7 +116,7 @@ class CouponController extends BaseController {
 			'isLive' => $isLive
 		));
 
-		{{file_put_contents('log_index.txt', json_encode($pick) . "\n" , FILE_APPEND | LOCK_EX) ;}}
+		{{file_put_contents('log_index.txt', json_encode($coupon) . "\n" , FILE_APPEND | LOCK_EX) ;}}
 		/*$view = View::make('bet.auto_form_selections', array('inputs' => $infos));
 		return $view;*/
 	}
