f5004ad18901756d61dc0b66102497c5602f00b9
unknown <slf.pro@gmail.com>
2015-05-22 04:37:59 +0200

    automatic form

--- app/controllers/CouponController.php
+++ app/controllers/CouponController.php
@@ -77,8 +77,9 @@ class CouponController extends BaseController {
 	}
 
 	public function getSelections(){
+
 		{{file_put_contents('log_index.txt', json_encode($_POST) . "\n" , FILE_APPEND | LOCK_EX) ;}}
-		{{file_put_contents('log_index.txt', json_encode($_GET) . "\n" , FILE_APPEND | LOCK_EX) ;}}
+		{{file_put_contents('log_index.txt', json_encode(Input::all()) . "\n" , FILE_APPEND | LOCK_EX) ;}}
 	}
 
 }
