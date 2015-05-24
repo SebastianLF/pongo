0afd2b4bf9cc2da95605ec8204de94f179bf7d36
zelol <slf.pro@gmail.com>
2015-05-24 10:01:02 +0200

    selections en session

--- public/js/pages/dashboard/paris/automaticBetForm.js
+++ public/js/pages/dashboard/paris/automaticBetForm.js
@@ -1,16 +1,15 @@
 //setInterval(refresh_selections, (5 * 1000));
-refresh_selections();
-function refresh_selections() {
-    $('#selection-refresh').click(function(e){
-        $.ajax({
-            url: 'selections',
-            success: function (data) {
-                $('#automatic-selections').html(data);
-            },
-            error: function (data) {
-                $('#automatic-selections').html('<p>impossible de récuperer les selections</p>');
-            }
-        });
+
+$('#selection-refresh').click(function (e) {
+    $.ajax({
+        url: 'selections',
+        success: function (data) {
+            $('#automatic-selections').html(data);
+        },
+        error: function (data) {
+            $('#automatic-selections').html('<p>impossible de récuperer les selections</p>');
+        }
     });
-}
+});
+
 
