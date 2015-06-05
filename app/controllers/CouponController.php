e38b3714c47f9648e2cf39bad98b290fb7baf5da
unknown <slf.pro@gmail.com>
2015-06-05 15:27:08 +0200

    automatic coupon

--- app/controllers/CouponController.php
+++ app/controllers/CouponController.php
@@ -127,6 +127,7 @@ class CouponController extends BaseController {
 
 		file_put_contents('log_index.txt', json_encode(Input::all()) . "\n" , FILE_APPEND | LOCK_EX);
 		file_put_contents('log_index.txt', json_encode($coupon) . "\n\n" , FILE_APPEND | LOCK_EX);
+		file_put_contents('log_index.txt', json_encode(Auth::user()) . "\n\n" , FILE_APPEND | LOCK_EX);
 
 		return 1;
 	}
