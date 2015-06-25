e36ee60a57646cca8c2953d9b6aa80b1bbd578a1
unknown <slf.pro@gmail.com>
2015-06-25 05:50:41 +0200

    automatic form

--- app/controllers/CouponController.php
+++ app/controllers/CouponController.php
@@ -206,14 +206,13 @@
 				'league_id' => $league_id,
 				'league_name' => $league_name,
 				'event_country_name' => $event_country_name,
-				'home_team' => $home_team,
+				/*'home_team' => $home_team,
 				'home_team_country_name' => $home_team_country_name,
 				'away_team' => $away_team,
 				'away_team_country_name' => $away_team_country_name,
-				/*'score' => $score,
+				'score' => $score,
 				'isLive' => $isLive == 'true' ? 1 : 0,
 				'isMatch' => $isMatch == 'true' ? 1 : 0,*/
-				'session_id' => $session_id,
 				'affichage' => $affichage_num
 			));
 			$coupon->save();
