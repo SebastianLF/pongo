fe7d510e64e584e32482872964975c2ea3067347
sebs-PORTABLE <slf.pro@gmail.com>
2015-09-03 16:10:04 +0200

    termine table + recap general

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -92,39 +92,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <script>
         jQuery(document).ready(function () {
 
-            var table = $('#table_id').DataTable( {
-                "ajax": "../ajax/data/objects.txt",
-                "columns": [
-                    {
-                        "className":      'details-control',
-                        "orderable":      false,
-                        "data":           null,
-                        "defaultContent": ''
-                    },
-                    { "data": "name" },
-                    { "data": "position" },
-                    { "data": "office" },
-                    { "data": "salary" }
-                ],
-                "order": [[1, 'asc']]
-            } );
 
-            // Add event listener for opening and closing details
-            $('#table_id tbody').on('click', 'td.details-control', function () {
-                var tr = $(this).closest('tr');
-                var row = table.row( tr );
-
-                if ( row.child.isShown() ) {
-                    // This row is already open - close it
-                    row.child.hide();
-                    tr.removeClass('shown');
-                }
-                else {
-                    // Open this row
-                    row.child( format(row.data()) ).show();
-                    tr.addClass('shown');
-                }
-            } );
 
             $('[data-toggle="tooltip"]').tooltip();
 
