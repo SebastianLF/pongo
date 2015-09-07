9fca1b0078621fd8598ba2fc1e2080ee7f698061
zelol <slf.pro@gmail.com>
2015-09-07 19:32:31 +0200

    termine table + recap general

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -9,16 +9,8 @@
 			$this->beforeFilter('csrf', array('on' => array('post', 'put', 'patch', 'delete')));
 			$this->beforeFilter('devise_missing', array('except' => array('postAutomaticSelections')));
 
-			$this->beforeFilter(function () {
-				Event::fire('clockwork.controller.start');
-			});
-
 			/*$getMetadataBag = Session::getMetadataBag();
 			Clockwork::info(time());
 			Clockwork::info(time() - $getMetadataBag->getLastUsed());*/
-
-			$this->afterFilter(function () {
-				Event::fire('clockwork.controller.end');
-			});
 		}
 	}
