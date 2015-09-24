7aece7085f823ee41716eda6b21b914c124a3df9
sebs-PORTABLE <slf.pro@gmail.com>
2015-09-24 16:18:09 +0200

    essai correction bug paris en cours enclose

--- app/lib/pari/PariAffichage.php
+++ app/lib/pari/PariAffichage.php
@@ -65,24 +65,24 @@ class PariAffichage implements PariAffichageInterface{
 
 
 		if($affichage_num == 1){return $market->name.' : '.$this->UniformiserNomEquipe($pick, $home_team, $away_team);}
-		elseif($affichage_num == 8){return $market->name.' : '.$pick;}
-		elseif($affichage_num == 2){return $market->name.' : '.$this->UniformiserNomEquipe($pick, $home_team, $away_team).' '.$oddParameter1;}
-		elseif($affichage_num == 3){return $market->representation.' : '.$this->UniformiserNomEquipe($pick, $home_team, $away_team).', '.$this->UniformiserNomEquipe($parameterName1, $home_team, $away_team);}
-		elseif($affichage_num == 4){return $market->name.' : '.$pick.', '.$oddParameter1.'-'.$oddParameter2.' minutes';}
-		elseif($affichage_num == 5){
+		elseif($affichage_num == 2){return $market->name.' : '.$pick;}
+		elseif($affichage_num == 3){return $market->name.' : '.$this->UniformiserNomEquipe($pick, $home_team, $away_team).' '.$oddParameter1;}
+		elseif($affichage_num == 4){return $market->name.' : '.$this->UniformiserNomEquipe($pick, $home_team, $away_team).', '.$parameterName1;}
+		elseif($affichage_num == 5){return $market->name.' : '.$pick.', '.$oddParameter1.'-'.$oddParameter2.' minutes';}
+		elseif($affichage_num == 6){
 			if($oddParameter1 > 0){
 				return $market->name.' : '.$pick.', '.$parameterName1.' +'.$oddParameter1;
 			}else{
 				return $market->name.' : '.$pick.', '.$parameterName1.' '.$oddParameter1;
 			}}
-		elseif($affichage_num == 6){return $market->name.' : '.$pick.' Top '.$oddParameter1;}
-		elseif($affichage_num == 7){
+		elseif($affichage_num == 7){return $market->name.' : '.$pick.' Top '.$oddParameter1;}
+		elseif($affichage_num == 8){
 			if($oddParameter1 > 0){
 				return $market->name.' : '.$this->UniformiserNomEquipe($pick, $home_team, $away_team).' +'.$oddParameter1;
 			}else{
 				return $market->name.' : '.$this->UniformiserNomEquipe($pick, $home_team, $away_team).' '.$oddParameter1;
 		}}
-		elseif($affichage_num == 8){return $market->name.' : '.$parameterName1.', '.$pick.' '.$oddParameter1;}
+		elseif($affichage_num == 9){return $market->name.' : '.$parameterName1.', '.$pick.' '.$oddParameter1;}
 
 		return 'Erreur affichage pari';
 	}
