e8ca57dff2cfaf31093184f51fa6392fb0621b48
sebs-PORTABLE <slf.pro@gmail.com>
2015-10-05 14:31:55 +0200

    table historique ladda + supprimer + export

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -338,6 +338,15 @@ License: You must have a valid license purchased only from themeforest(the above
     var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(glcspt, s);
 </script>
 <!-- End of ClickDesk -->
+<!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+    window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+            d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+            _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+        $.src="//v2.zopim.com/?2yLYoTe7KZc5eXH3mViXVyumTMmOeU8a";z.t=+new Date;$.
+                type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zopim Live Chat Script-->
     @show
 
 
