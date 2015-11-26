99ec85a34ac0a744ac133d1ac94b414bacae7290
sebs-PORTABLE <slf.pro@gmail.com>
2015-11-26 17:39:28 +0100

    version fonctionnelle v0.1

--- app/database/attente/2015_11_07_112437_add_isoutright_to_coupon.php
+++ app/database/attente/2015_11_07_112437_add_isoutright_to_coupon.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+	use Illuminate\Database\Migrations\Migration;
+	use Illuminate\Database\Schema\Blueprint;
 
-class AddIsOutrightToCoupon extends Migration {
+	class AddIsOutrightToCoupon extends Migration {
 
 	/**
 	 * Run the migrations.
